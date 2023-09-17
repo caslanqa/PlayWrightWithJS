b4e2f22f756060f73cf89b1e40cb8f16a862cb48
caslanqa <cihan.aslan.qa@gmail.com>
2023-09-17 19:16:19 +0300

    runapitest.sh updated

--- runapitest.sh
+++ runapitest.sh
@@ -8,16 +8,4 @@ npm i -D @playwright/test
 
 wait
 
-npm i allure-playwright
-
-wait
-
-npm install -g allure-commandline --save-dev
-
-wait
-
-npx playwright test API_TEST/$1.spec.js --project=chromium --headed --reporter=allure-playwright
-
-wait
-
-allure generate allure-results -o allure-report --clean
+npx playwright test API_TEST/$1.spec.js
