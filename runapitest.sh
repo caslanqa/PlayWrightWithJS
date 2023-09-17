455d5e1289e42fd5cdb5119fa41cd3652895999d
caslanqa <cihan.aslan.qa@gmail.com>
2023-09-17 10:47:11 +0300

    apishellcommand

--- runapitest.sh
+++ runapitest.sh
@@ -1,14 +1,5 @@
 #! /usr/bin/bash
-rm -r allure-results | rm -r allure-report
 
 wait
 
-npx playwright test API_TEST/$1.spec.js --project=chromium --headed --reporter=allure-playwright
-
-wait
-
-allure generate allure-results -o allure-report --clean
-
-wait
-
-allure open allure-report
+npx playwright test API_TEST/$1.spec.js --project=chromium --headed
